66967179fe62a4466b4ca95fcb16ba871cd30a5a
leysanchezb92 <leysanchezb92@hotmail.com>
2021-07-12 14:48:05 -0500

    adding main page to website, assets and router dependency

--- strategy/src/App.js
+++ strategy/src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
+import logo from './assets/strategy.svg';
 
 function App() {
   return (
-    <h1>Hola mundo</h1>
+    <div className="main">
+      <div className="main logo">
+        <img src={logo} alt="Logo Strategy"/>
+        <h1>Strategy</h1>
+        <p>Organize and set your pieces, whenever and whatever you want</p>
+      </div>
+      <div className="buttons">
+        <button id="register" className="btn-general">Register</button>
+        <button id="login" className="btn-general">login</button>
+      </div>
+    </div>
   );
 }
 
